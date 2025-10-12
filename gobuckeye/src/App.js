bdef8873c800bebe508f9791a498fb13d1dee004
zhang_legion_GitHub_Desktop <zhang_legion@githubdesktop.com>
2025-10-12 18:11:14 -0400

    added purchase.js, and images folder, and Lab5 documents updated App.js,

--- gobuckeye/src/App.js
+++ gobuckeye/src/App.js
@@ -1,21 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Purchase from './components/purchase';
 import ViewOrder from './components/viewOrder';
-import ViewConfirmatoin from './components/viewConfirmation';
+import ViewConfirmation from './components/viewConfirmation';
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      <div className='content'>
-        <Routes>
-          <Route path='/purchase/viewOrder' element={<ViewOrder/>} />
-          <Route path='/purchase/viewConfirmation' element={<ViewConfirmatoin/>} />
-        </Routes>
-      </div>
-    </Router>
+      <Router>
+        <div className='content'>
+          <Routes>
+            {/* Redirect root to purchase page */}
+            <Route path='/' element={<Navigate to='/purchase' replace />} />
+            
+            {/* Main purchase/catalog page */}
+            <Route path='/purchase' element={<Purchase />} />
+            
+            {/* Order review page */}
+            <Route path='/purchase/viewOrder' element={<ViewOrder />} />
+            
+            {/* Order confirmation page */}
+            <Route path='/purchase/viewConfirmation' element={<ViewConfirmation />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
